2fe43d1b1d21536fccf0303da345929d44959d7a
Sally Lehman <sally.lehman@auth0.com>
2017-04-04 02:19:31 +0000

    missed the fix enabled line

--- spec/puppet-lint/plugins/check_whitespace/resource_whitespace_spec.rb
+++ spec/puppet-lint/plugins/check_whitespace/resource_whitespace_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe 'resource_whitespace' do
   let(:msg) { 'incorrect resource declaration spacing found' }
 
-   context 'resource with wrong number of spaces between resource type declaration and bracket' do
+  context 'with fix disabled' do
+   context 'incorrect spacing around resource type' do
       let(:code) { "
         file{ '/tmp/bad2': ensure => file; }
         file   { '/tmp/bad5': ensure => file; }"
@@ -29,28 +30,29 @@ describe 'resource_whitespace' do
       PuppetLint.configuration.fix = false
     end
 
-  context 'resource with wrong number of spaces between resource type declaration and bracket' do
-    let(:code) { "
-      file{ '/tmp/bad2': ensure => file; }
-      file   { '/tmp/bad5': ensure => file; }"
-    }
+    context 'incorrect spacing around resource type' do
+      let(:code) { "
+        file{ '/tmp/bad2': ensure => file; }
+        file   { '/tmp/bad5': ensure => file; }"
+      }
     
-    let(:fixed) { "
-      file { '/tmp/bad2': ensure => file; }
-      file { '/tmp/bad5': ensure => file; }"
-    }
+      let(:fixed) { "
+        file { '/tmp/bad2': ensure => file; }
+        file { '/tmp/bad5': ensure => file; }"
+      }
       
-    it 'should detect two problems' do
-      expect(problems).to have(2).problem
-    end
+      it 'should detect two problems' do
+        expect(problems).to have(2).problem
+      end
 
-    it 'should create two resource type declaration warnings' do
-      expect(problems).to contain_fixed(msg).on_line(1).in_column(13)
-      expect(problems).to contain_fixed(msg).on_line(2).in_column(14)
-    end
+      it 'should create two warnings' do
+        expect(problems).to contain_fixed(msg).on_line(1).in_column(13)
+        expect(problems).to contain_fixed(msg).on_line(2).in_column(14)
+      end
 
-    it 'should adjust incorrect resource whitespace' do
-      expect(manifest).to eq(fixed)
+      it 'should adjust incorrect resource whitespace' do
+        expect(manifest).to eq(fixed)
+      end
     end
   end
 end
